3ed7db987481eff1c6e2d7173013f07708776136
Sanchay Krishna <sanchaykrishna15@gmail.com>
2026-03-04 16:57:33 +0530

    Fix clinic routing build error

--- src/app/[clinic]/page.tsx
+++ src/app/[clinic]/page.tsx
@@ -3,10 +3,11 @@ import ChatPage from "../page"
 export default function ClinicPage({ params }: { params: { clinic: string } }) {
 
   const clinic = params.clinic
+  const Page = ChatPage as any
 
   return (
     <div>
-      <ChatPage clinic={clinic} />
+      <Page clinic={clinic} />
     </div>
   )
 }
